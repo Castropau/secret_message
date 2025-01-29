1069fa901507e64820e532914c265956e88b27fc
Paulo Castro <castro.paulo5.00@gmail.com>
2025-01-29 15:58:40 +0800

    samplee

--- app/utils/page.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-
-const supabaseUrl = 'https://lcuiwaljmzgkockdayer.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjdWl3YWxqbXpna29ja2RheWVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwNDk2MTUsImV4cCI6MjA1MzYyNTYxNX0.HtvKOr5Q3rLeClm53dufQ9hdjpCvD55WbH7C_omnQQY';
-const Supabase = createClient(supabaseUrl, supabaseKey);
-
-export default Supabase;
